983b66172fc85719cdab785ae390b41d3d7139c4
johnnyyork13 <johnnyyork13@hotmail.com>
2022-12-26 20:01:26 -0500

    minor adjustments

--- public/javascript.js
+++ public/javascript.js
@@ -1,16 +1,18 @@
-function mouseover()
-{
-    const idList = document.querySelectorAll("#profileHeader, #phoneHeader, #emailHeader, #webHeader, #educationHeader, #experienceHeader, #knowledgeHeader");
+const idList = document.querySelectorAll("#profileHeader, #phoneHeader, #emailHeader, #webHeader, #educationHeader, #experienceHeader, #knowledgeHeader");
 
+
+function mouseover(idList)
+{
     for (i = 0; i <= idList.length - 1; i++)
     {
-        const test = idList[i];
-        test.addEventListener('mouseenter', function(){
-            test.style.color = "black";        })
-        test.addEventListener('mouseleave', function(){
-            test.style.color = "white";
+        const item = idList[i]
+        item.addEventListener('mouseenter', function(){
+            item.style.color = "#03254c";
+        })
+        item.addEventListener('mouseleave', function(){
+            item.style.color = "white";
         })
     }
 }
 
-mouseover()
+mouseover(idList)
