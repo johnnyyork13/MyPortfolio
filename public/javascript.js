1cd3d4fab73bdce1a3671128b7879c77ab06e544
johnnyyork13 <johnnyyork13@hotmail.com>
2022-12-27 19:11:16 -0500

    add some transitional effects

--- public/javascript.js
+++ public/javascript.js
@@ -15,4 +15,22 @@ function mouseover(idList)
     }
 }
 
+const profileHeader = document.getElementById('profileHeader');
+let hiddenDiv = document.getElementById('hiddenDiv');
+
+profileHeader.addEventListener('click', function(){
+    hiddenDiv.style.visibility = 'visible'; 
+    hiddenDiv.style.width = '900px';
+    hiddenDiv.style.height = '900px';
+    if (hiddenDiv.style.width == '900px' && hiddenDiv.style.visibility == 'visible')
+    {
+        const closeDiv = document.getElementById('closeProfileButton')
+        closeDiv.addEventListener('click', function(){
+        hiddenDiv.style.visibility = 'hidden';
+        hiddenDiv.style.width = '0px';
+        hiddenDiv.style.height = '0px';
+    })
+    }
+})
+
 mouseover(idList)
