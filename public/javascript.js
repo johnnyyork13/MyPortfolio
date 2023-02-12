501eeec86460f0d873e3b95ff6cce6a9c06314d0
johnnyyork13 <johnnyyork13@hotmail.com>
2023-02-12 13:13:04 -0500

    change theme and add sections

--- public/javascript.js
+++ public/javascript.js
@@ -1,14 +1,28 @@
-const mainDiv = document.querySelectorAll('.container');
-const sideBar = document.querySelectorAll('.sidePara');
+const linkP = document.getElementsByClassName('linkP');
+const mainDiv = document.getElementsByClassName('mainDiv');
 
-function addListeners()
-{
-    sideBar.forEach((para) => {
-        para.addEventListener('mouseover', showElement(para.id));
-    })
+let linkArray = [];
+
+for (i = 0; i < linkP.length; i++) {
+    let e = linkP[i];
+    e.addEventListener('click', openSection);
 }
 
+function openSection(){
+    for (i = 0; i < mainDiv.length; i++) {
+        let e = mainDiv[i];
+        if (e.classList[0] === this.id) {
+            e.style.visibility = 'visible';
+            e.children[0].className = "w3-center w3-animate-right";
+            e.children[1].className = "w3-center w3-animate-right";
+        } else {
+            e.style.visibility = 'hidden';
+            e.children[0].className = "";
+            e.children[1].className = "";
+        }
 
-
-
-addListeners();
+        //Style List
+        this.parentElement.parentElement.children[i].children[0].style.fontSize = '1em';    
+    }
+    this.style.fontSize = '1.5em';
+}
